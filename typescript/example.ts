4cdfc98704ecd59201329bd1af176387e5c9db0b
Floyd May <floyd@canyon-trail.com>
2021-12-01 13:24:21 -0600

    some niceties

--- typescript/example.ts
+++ typescript/example.ts
@@ -2,4 +2,6 @@ export default function example(x: number, y: number) {
 	return x + y;
 }
 
-console.log(`2 + 2 = ${example(2, 2)}`);
+if(require.main === module) {
+  console.log(`2 + 2 = ${example(2, 2)}`);
+}
